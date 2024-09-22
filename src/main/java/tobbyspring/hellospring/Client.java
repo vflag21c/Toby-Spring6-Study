47fd24469253c79d3501169521677c40622d549f
YouYeol <vflag21c@nate.com>
2024-09-22 12:50:43 +0900

    토비의 스프링 6 이해와 원리 학습 ( ~ 31.03% )

---
+++ src/main/java/tobbyspring/hellospring/Client.java
@@ -0,0 +1,13 @@
+package tobbyspring.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        ObjectFactory objectFactory = new ObjectFactory();
+        PaymentService paymentService = objectFactory.paymentService();
+        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
+        System.out.println(payment);
+    }
+}
---
+++ src/main/java/tobbyspring/hellospring/ExRateProvider.java
@@ -0,0 +1,8 @@
+package tobbyspring.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public interface ExRateProvider {
+    BigDecimal getExRate(String currency) throws IOException;
+}
